e3afecd9e6f866741df328438b678db9297c0312
heiheidandan <125861457+heiheidandan@users.noreply.github.com>
2023-02-23 01:33:22 +0800

    Update ld1.js

--- xzb/ld1.js
+++ xzb/ld1.js
@@ -3,7 +3,7 @@ document.writeln("<center>");
 document.writeln("			<a href=\"http://service-6tulm0hs-1313409281.gz.apigw.tencentcs.com\"><img src=\"http://123.186.147.14:83/1.jpg\" width=\"100%\" border=\"边框\"></a>			</center>");
 document.write("           <br \/>");
 document.writeln("            <center>");
-document.writeln("			<a href=\"http://service-6tulm0hs-1313409281.gz.apigw.tencentcs.com\"><img src=\"http://123.186.147.14:83/2.jpg\/zz\/2.jpg\" width=\"100%\" border=\"边框\"></a>			</center>");
+document.writeln("			<a href=\"http://service-6tulm0hs-1313409281.gz.apigw.tencentcs.com\"><img src=\"http://123.186.147.14:83/2.jpg\" width=\"100%\" border=\"边框\"></a>			</center>");
 document.write("           <br \/>");
 document.write("            <br \/>");
 document.write("          <br \/>");
